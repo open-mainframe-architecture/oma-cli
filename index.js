c96b427a1c863f78ce334dea3df60ab9b312900e
highdutch <dick.heijink@gmail.com>
2016-04-27 23:39:09 +0200

    es6

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 "use strict";
 
-var path = require('path');
+const path = require('path');
 
-var yargs = require('yargs');
+const yargs = require('yargs');
 
-module.exports = function (version, commandSpecs, alternativeCommandLine) {
+module.exports = (version, commandSpecs, alternativeCommandLine) => {
   // assume main module is a script in bin/ directory
-  var main = path.basename(require.main.filename);
+  const main = path.basename(require.main.filename);
   // start with arguments for unparsed command with unknown options
-  var mainArgs = yargs.strict().help('help').version(version)
+  const mainArgs = yargs.strict().help('help').version(version)
     .usage('Usage: ' + main + ' <command> [options]')
     .epilogue('Supply <command> for more help');
   // keep track whether a command has been parsed
-  var commandKey = null;
+  let commandKey = null;
   // specify commands to parse
-  for (var key in commandSpecs) {
-    mainArgs.command(key, commandSpecs[key].short, function () {
+  for (let key in commandSpecs) {
+    mainArgs.command(key, commandSpecs[key].short, function() {
       // when the callback is called, this command has been parsed
-      var commandSpec = commandSpecs[commandKey = this];
+      const commandSpec = commandSpecs[commandKey = this];
       // specify options of this command (as expected by yargs)
-      var yopts = {}, nargs = {};
-      var least = typeof commandSpec.least === 'number' ? commandSpec.least : 0;
-      var most = typeof commandSpec.most === 'number' ? commandSpec.most : -1;
-      var examples = commandSpec.examples || [];
-      var usage = commandSpec.usage;
-      var long = commandSpec.long;
-      for (var alias in commandSpec.option) {
-        var optionSpec = commandSpec.option[alias];
-        var letter = optionSpec.letter, arity = optionSpec.arity;
+      const yopts = {}, nargs = {};
+      const least = typeof commandSpec.least === 'number' ? commandSpec.least : 0;
+      const most = typeof commandSpec.most === 'number' ? commandSpec.most : -1;
+      const examples = commandSpec.examples || [];
+      const usage = commandSpec.usage, long = commandSpec.long;
+      for (let alias in commandSpec.option) {
+        const optionSpec = commandSpec.option[alias];
+        const letter = optionSpec.letter, arity = optionSpec.arity;
         if (yopts[letter]) {
-          throw 'Duplicate letter ' + letter + ' in configuration of command ' + commandKey;
+          throw new Error(`Duplicate letter ${letter} in configuration of command ${commandKey}`);
         }
         nargs[letter] = arity === 0 ? 0 : arity || 1;
         yopts[letter] = {
@@ -40,46 +39,45 @@ module.exports = function (version, commandSpecs, alternativeCommandLine) {
         };
       }
       // reset arguments for parsed command with known options
-      var yusage = 'Usage: ' + main + ' ' + commandKey + ' ' + usage;
+      const yusage = `Usage: ${main} ${commandKey} ${usage}`;
       mainArgs.reset().strict().help('help').nargs(nargs).options(yopts).usage(yusage);
       if (least > 0 || most >= least) {
         mainArgs.demand(least + 1, most >= least ? most + 1 : void 0);
       }
-      examples.forEach(function (example) {
-        mainArgs.example(main + ' ' + commandKey + ' ' + example, '');
-      });
+      for (let example of examples) {
+        mainArgs.example(`${main} ${commandKey} ${example}`, '');
+      }
       if (long) {
         mainArgs.epilogue(long);
       }
     }.bind(key));
   }
   // parse command line to trigger callback
-  var argv = alternativeCommandLine ? mainArgs.parse(alternativeCommandLine) : mainArgs.argv;
-  var firstNonOption = argv._[0];
+  const argv = alternativeCommandLine ? mainArgs.parse(alternativeCommandLine) : mainArgs.argv;
+  const firstNonOption = argv._[0];
   if (!commandKey || firstNonOption !== commandKey) {
     mainArgs.showHelp();
     if (commandKey && firstNonOption) {
       // this quirk happens when some non-option is accidentally parsed as the command
-      console.error('Command ambiguity in non-option: ' + commandKey + ' vs. ' + firstNonOption);
+      console.error(`Command ambiguity in non-option: ${commandKey} vs. ${firstNonOption}`);
     } else if (firstNonOption) {
-      console.error('Unknown command: ' + firstNonOption);
+      console.error(`Unknown command: ${firstNonOption}`);
     }
     return;
   }
   // extract command options from parse results
-  var commandSpec = commandSpecs[commandKey];
-  var opts = { '': argv._.slice(1) };
-  for (var key in argv) {
-    var optionSpec = commandSpec.option[key];
+  const commandSpec = commandSpecs[commandKey], opts = { '': argv._.slice(1) };
+  for (let key in argv) {
+    const optionSpec = commandSpec.option[key];
     if (optionSpec) {
       if (optionSpec.arity === 0) {
         opts[key] = argv[key];
       } else {
-        var values = Array.isArray(argv[key]) ? argv[key] : [argv[key]];
-        var arity = optionSpec.arity || 1;
+        const values = Array.isArray(argv[key]) ? argv[key] : [argv[key]];
+        const arity = optionSpec.arity || 1;
         if (optionSpec.once && values.length > arity) {
           mainArgs.showHelp();
-          console.error('Too many options: ' + key);
+          console.error(`Too many options: ${key}`);
           return;
         }
         opts[key] = optionSpec.once && arity === 1 ? values[0] : values;
@@ -87,10 +85,6 @@ module.exports = function (version, commandSpecs, alternativeCommandLine) {
     }
   }
   // perform asynchronous command with extracted options
-  var command = commandSpec.command;
-  command(opts)
-    .then(null, function (uncaught) {
-      console.error(uncaught.stack ? uncaught.stack : uncaught);
-    })
-  ;
+  const command = commandSpec.command;
+  command(opts).catch(uncaught => { console.error(uncaught.stack ? uncaught.stack : uncaught); });
 };
